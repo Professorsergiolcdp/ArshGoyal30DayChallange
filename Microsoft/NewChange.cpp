775775fe638b9c053f75c5a08dbda60a7adf7b3b
Professorsergiolcdp <professorsergioldp@gmail.com>
2023-01-06 12:12:50 +0530

    Microsoft

---
+++ Microsoft/NewChange.cpp
@@ -0,0 +1,7 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout<<"This the Microsoft Challenge Set up by Arsh Goyal Thank you the time that you invset on us";
+    return 0;
+}
